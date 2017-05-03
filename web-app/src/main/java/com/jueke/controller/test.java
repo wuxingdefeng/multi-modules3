624d667a6760a064cde84a690ccef7149b2c1587
oudezhi <ouyang_riguang@163.com>
2017-05-03 10:32:17 +0800

    冒泡测试

--- web-app/src/main/java/com/jueke/controller/test.java
+++ web-app/src/main/java/com/jueke/controller/test.java
@@ -1,17 +1,29 @@
 package com.jueke.controller;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
  * Created by Administrator on 2017/3/30.
  */
 public class test {
     public static void main(String[] args) {
-                ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-                Math math = (Math) ctx.getBean("math");
+                //ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+               // Math math = (Math) ctx.getBean("math");
               //  userService.add();
-                 math.add(1,2);
+               //  math.add(1,2);
+        int[] qb ={32,34,12,77,54,28,65,9,73,93,23};
+        for(int i=0;i<qb.length-1;i++){
+            for(int j=i;j<qb.length-1;j++){
+                int temp =qb[j+1];
+                if(temp>qb[i]){
+                    qb[j+1]=qb[i];
+                    qb[i]=temp;
+                }
+            }
+        }
+
+
+        for(int z:qb){
+            System.out.println(z);
+        }
 
     }
 }
